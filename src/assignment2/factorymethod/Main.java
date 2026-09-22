bacf1cb0f8dfb061ebfe24e3c5448f11c4d17615
kashakbayev <114176812+kashakbayev@users.noreply.github.com>
2026-09-22 14:23:15 +0500

    factory method is ready

---
+++ src/assignment2/factorymethod/Main.java
@@ -0,0 +1,19 @@
+package assignment2.factorymethod;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        DrinkMaker a = new EspressoMaker();
+        DrinkMaker b = new LatteMaker();
+        DrinkMaker c = new CappuccinoMaker();
+
+        a.serve();
+        System.out.println();
+
+        b.serve();
+        System.out.println();
+
+        c.serve();
+    }
+
+}
